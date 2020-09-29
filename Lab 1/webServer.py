8a90c0ab649eeaa181574e6ab5da54d3add09d3d
Gary C <garychang9@gmail.com>
2020-09-29 02:56:38 -0700

    finished lab 1

--- Lab 1/webServer.py
+++ Lab 1/webServer.py
@@ -1,36 +1,42 @@
 from socket import * # import socket module
 import sys # In order to terminate the program
 
+# Create TCP socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
-# Prepare a sever socket
-# Fill in start
-# Fill in end
+
+# TODO: Prepare a sever socket
+PORT = 80
+serverSocket.bind(('', PORT))
+serverSocket.listen(1)
+print('web server is listening on port:', PORT)
+
 while True:
-    # Establish the connection
+    # TODO: connectionSocket, addr = ?
     print('Ready to serve...')
-    connectionSocket, addr = # Fill in start # Fill in end
+    connectionSocket, addr = serverSocket.accept()
+    
     try:
-        message = #Fill in start #Fill in end
+        # TODO: message = ? and outputdata = ?
+        message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start #Fill in end
-        #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
-        #Send the content of the requested file to the client
+        outputdata = f.read()
+        
+        # TODO: Send one HTTP header line into socket
+        connectionSocket.send(b'\nHTTP/1.1 200 OK\n\n')
+        
+        # Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send(outputdata[i].encode('utf-8'))
         connectionSocket.send("\r\n".encode())
 
         connectionSocket.close()
     except IOError:
-        #Send response message for file not found
-        #Fill in start 
-        #Fill in end
-        #Close client socket
-        #Fill in start
-        #Fill in end
-
+        # TODO: Send response message for file not found
+        connectionSocket.send(b'Error 404: File not found')
+        
+        # TODO: Close client socket
+        connectionSocket.close()
 serverSocket.close()
 
 #Terminate the program after sending the corresponding data
