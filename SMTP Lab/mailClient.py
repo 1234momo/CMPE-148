55fcb9edb654fb44a4257799d36ce742b36df77f
Gary C <garychang9@gmail.com>
2020-10-12 17:56:22 -0700

    finished SMTP lab

--- SMTP Lab/mailClient.py
+++ SMTP Lab/mailClient.py
@@ -1,21 +1,21 @@
 from socket import *
-import base64
 
-msg = "\r\n I love computer networks!"
+msg = "Hello from Hamsika, William, Donie, and Gary\n😭😂😂😭\r\n"
 endmsg = "\r\n.\r\n"
+
 
 # TODO: Choose a mail server (e.g. Google mail server) and call it mailserver
 mailserver = ("mail.smtp2go.com", 2525)
 
-# TODO: Create socket called clientSocket and establish a TCP connection with mailserver
+
+# TODO: Create socket called clientSocket and establish a TCP connection with mail server
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect(mailserver)
-
 recv = clientSocket.recv(1024).decode()
 print(recv)
-
 if recv[:3] != '220':
     print('220 reply not received from server.')
+   
     
 # Send HELO command and print server response.
 heloCommand = 'HELO Alice\r\n'
@@ -24,40 +24,25 @@ recv1 = clientSocket.recv(1024).decode()
 print(recv1)
 if recv1[:3] != '250':
     print('250 reply not received from server.')
-    
-# Using TLS for requests
-# tls = "STARTTLS\r\n"
-# clientSocket.send(tls.encode())
-# recv_tls = clientSocket.recv(1024).decode()
-# print(recv_tls)
-# if recv1[:3] != '250':
-#    print('250 reply not received from server.')
 
-# gmail authentication
-# username = "email"
-# pwd = "pwd"
-# base64_str = ("\x00" + username + "\x00" + pwd).encode()
-# base64_str = base64.b64encode(base64_str)
-# authMsg = "AUTH PLAIN ".encode() + base64_str + "\r\n".encode()
-# clientSocket.send(authMsg)
-# recv_auth = clientSocket.recv(1024)
-# print(recv_auth.decode())
 
 # TODO: Send MAIL FROM command and print server response.
-mailFrom = "MAIL FROM: <email> \r\n"
+mailFrom = "MAIL FROM: <garychang9@gmail.com> \r\n"
 clientSocket.send(mailFrom.encode())
 recv2 = clientSocket.recv(1024).decode()
 print("MAIL FROM response: " + recv2)
 if recv1[:3] != '250':
     print('250 reply not received from server.')
 
+
 # TODO: Send RCPT TO command and print server response. 
-rcptTo = "RCPT TO: <email> \r\n"
-clientSocket.send(rcptTo.encode()) # ERROR: 550 relay access denied - please authenticate. 
+rcptTo = "RCPT TO: <gary.chang@sjsu.edu> \r\n"
+clientSocket.send(rcptTo.encode())
 recv3 = clientSocket.recv(1024).decode()
 print("RCPT TO response: " + recv3)
 if recv1[:3] != '250':
     print('250 reply not received from server.')
+
 
 # TODO: Send DATA command and print server response. 
 data = "DATA\r\n"
@@ -67,19 +52,17 @@ print("DATA response: " + recv4)
 if recv1[:3] != '250':
     print('250 reply not received from server.')
 
-# # TODO: Send message data.
-# clientSocket.send(msg.encode())
-# recv_msg = clientSocket.recv(1024).decode()
-# print("Response after sending message body: " + recv_msg)
-# if recv1[:3] != '250':
-#     print('250 reply not received from server.')
 
-# # TODO: Message ends with a single period.
-# clientSocket.send(endmsg.encode())
-# recv_msg_end = clientSocket.recv(1024)
-# print("msg end: " + recv_msg_end.decode())
-# if recv1[:3] != '250':
-#     print('250 reply not received from server.')
+# TODO: Send message data and a single period denoting message ending
+subject = "Subject: SMTP lab test \r\n\r\n" 
+clientSocket.send(subject.encode())
+clientSocket.send(msg.encode())
+clientSocket.send(endmsg.encode())
+recv_msg = clientSocket.recv(1024)
+print("Response after sending message body:" + recv_msg.decode())
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
     
 # TODO: Send QUIT command and get server response.
 clientSocket.send("QUIT\r\n".encode())
